5f37177cea97d03693677c9825893c7e2d8c0969
Triasss <ivan.trias@inslapineda.cat>
2025-09-25 10:22:30 +0200

    Migrations #1

---
+++ database/migrations/2025_09_24_111058_create_experiences_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->id();
+            $table->string('position');
+            $table->string('company');
+            $table->text('description')->nullable();
+            $table->timestamp('start_date');
+            $table->timestamps('end_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('experiences');
+    }
+};
