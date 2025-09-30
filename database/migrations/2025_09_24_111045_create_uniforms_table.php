ee343766ab7f6714acd4b4dd5b62043f97adec2e
Triasss <ivan.trias@inslapineda.cat>
2025-09-30 12:43:56 +0200

    BBDD + Models #1

--- database/migrations/2025_09_24_111045_create_uniforms_table.php
+++ database/migrations/2025_09_24_111045_create_uniforms_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('resources', function (Blueprint $table) {
+        Schema::create('uniforms', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('given_by_user_id');
             $table->unsignedBigInteger('user_id');
