8cb47dc63645743c6ffa03f9951fffb0e507099f
LaiaMartin03 <laia.martin@inslapineda.cat>
2025-12-03 13:18:30 +0100

    Muchas cosas #4

--- resources/views/components/icons.blade.php
+++ resources/views/components/icons.blade.php
@@ -43,6 +43,10 @@
       <path stroke-linecap="round" stroke-linejoin="round" fill="none" stroke-width="1.5" stroke="currentColor" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
     </symbol>
 
+    <symbol id="new-tab" viewBox="0 0 24 24">
+        <path fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+    </symbol>
+
     <symbol id="userminus" viewBox="0 0 24 24">
         <path stroke-linecap="round" stroke-linejoin="round" fill="none" stroke-width="1.5" stroke="currentColor"d="M22 10.5h-6m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM4 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 10.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
     </symbol>
--- resources/views/outsiders/index.blade.php
+++ resources/views/outsiders/index.blade.php
@@ -11,21 +11,23 @@
         @if($outsiders->isEmpty())
             <p>Ni hi han contactes registrats.</p>
 
-            <div class="rounded-xl bg-white flex flex-col p-5 w-full shadow-[5px_5px_15px_2px_rgba(0,0,0,0.12)] gap-3">
-                <span>Antonia Lopez</span>
-                <a class="" href="">
-                    <svg class="size-5 text-gray-300">
-                        <use href="#new-tab" />
-                    </svg>
-                </a>
+            <div class="rounded-xl bg-white flex flex-col p-5 w-fit shadow-[5px_5px_15px_2px_rgba(0,0,0,0.12)]">
+                <div class="mb-3 gap-16 flex items-center">
+                    <span>Antonia Lopez</span>
+                    <button class="">
+                        <svg class="size-5 text-gray-300">
+                            <use href="#new-tab" />
+                        </svg>
+                    </button>
+                </div>
                 <span>antonia@gmail.com</span>
-                <span>666 66 66 66</span>
-                <span>Limpieza</span>
+                <span class="mb-3">666 66 66 66</span>
+                <span class="text-sm text-primary_color text-right">Limpieza</span>
             </div>
         @else
-            <div clasS="grid grid-cols-5 grid-rows-auto gap-16">
+            <div class="grid grid-cols-5 grid-rows-auto gap-16">
                 @foreach($outsiders as $outsider)
-                    <div class="rounded-xl bg-white flex flex-col p-5 w-full shadow-[5px_5px_15px_2px_rgba(0,0,0,0.12)] gap-3">
+                    <div class="rounded-xl bg-white flex flex-col p-5 w-fit shadow-[5px_5px_15px_2px_rgba(0,0,0,0.12)] gap-3">
                         <span>Antonia Lopez</span>
                         <a class="" href="{{ route('outsider.show', $outsider->id) }}">
                             <svg class="size-5 text-gray-300">
