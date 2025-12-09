418374a9646de524447e6b9a624086b548afb41d
Triasss <ivan.trias@inslapineda.cat>
2025-12-09 09:31:16 +0100

    Revert "Merge branch 'main' into IvanTrias/RRHH"
    This reverts commit f284d84be31b2c0bc98d8f26814d9efe7559a525, reversing
    changes made to e992c0884ae4ff75c922fce84fee4ad5f26206b7.

--- routes/web.php
+++ routes/web.php
@@ -92,6 +92,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::post('/save-drag-drops', [LearningProgramController::class, 'saveDragDrops']);
 
+    Route::post('/save-drag-drops', [LearningProgramController::class, 'saveDragDrops']);
+
     //Contactes externs
     Route::resource('outsiders', OutsiderController::class);
     Route::get('/outsiders/edit', [OutsiderController::class, 'edit'])
